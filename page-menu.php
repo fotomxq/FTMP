745b87efb17eed9ad3816a7f1443becb00b33e34
fotomxq <fotomxq@gmail.com>
2014-04-06 11:21:46 +0800

    美化中心页面 * 添加小图标 * 添加Logo

--- page-menu.php
+++ page-menu.php
@@ -2,7 +2,7 @@
 /**
  * 页面-菜单部分
  * @author fotomxq <fotomxq.me>
- * @version 1
+ * @version 2
  * @package web
  */
 if(!isset($pageArr)) die();
@@ -16,14 +16,14 @@ if(!isset($pageArr)) die();
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php"><?php echo $webData['WEB-TITLE'].' '.$pageArr['title']; ?></a>
+          <a class="navbar-brand" href="index.php"><img src="includes/assets/imgs/favicon.png" class="logo"> <?php echo $webData['WEB-TITLE'].' '.$pageArr['title']; ?></a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li<?php if($pageArr['menu-focus'] == 'center'){echo ' class="active"';} ?>><a href="center.php">Center</a></li>
-            <li<?php if($pageArr['menu-focus'] == 'user'){echo ' class="active"';} ?>><a href="center-user.php">User</a></li>
-            <li<?php if($pageArr['menu-focus'] == 'operate'){echo ' class="active"';} ?>><a href="center-operate.php">Operate</a></li>
-            <li><a href="action-logout.php">Logout</a></li>
+            <li<?php if($pageArr['menu-focus'] == 'center'){echo ' class="active"';} ?>><a href="center.php"><span class="glyphicon glyphicon-home"></span> Center</a></li>
+            <li<?php if($pageArr['menu-focus'] == 'user'){echo ' class="active"';} ?>><a href="center-user.php"><span class="glyphicon glyphicon-credit-card"></span> User</a></li>
+            <li<?php if($pageArr['menu-focus'] == 'operate'){echo ' class="active"';} ?>><a href="center-operate.php"><span class="glyphicon glyphicon-certificate"></span> Operate</a></li>
+            <li><a href="action-logout.php"><span class="glyphicon glyphicon-remove-sign"></span> Logout</a></li>
           </ul>
         </div>
       </div>

18470dc57ea968d9c3b91da622dae89d0898201a
fotomxq <fotomxq@gmail.com>
2014-04-08 11:25:28 +0800

    修正登录模块 * 修正登录检测模块重复登录错误

--- action-logged.php
+++ action-logged.php
@@ -1,29 +1,29 @@
 <?php
+
 /**
  * 登录检测模块
  * @author fotomxq <fotomxq.me>
- * @version 1
+ * @version 2
  * @package web
  */
-
 //引用全局
-require(dirname(__FILE__).DIRECTORY_SEPARATOR.'glob.php');
+require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'glob.php');
 
 //判断是否已经登录
 $userID = $user->logged($ipAddr);
-if($userID < 1){
-	//如果没有登录，跳转到登录页面
-	CoreHeader::toURL('index.php');
+if ($userID < 1) {
+    //如果没有登录，跳转到登录页面
+    CoreHeader::toURL('index.php?nologin');
 }
 
 //获取用户权限判断组
-$userPowers = $user->checkPower($userID,$user->powerValues);
+$userPowers = $user->checkPower($userID, $user->powerValues);
 
 //判断用户是否具备基本权限
-if(!$userPowers){
-	CoreHeader::toURL('index.php?power');
+if (!$userPowers) {
+    CoreHeader::toURL('index.php?power');
 }
-if(!$userPowers['NORMAL']){
-	die('No Power.');
+if (!$userPowers['NORMAL']) {
+    die('No Power.');
 }
 ?>

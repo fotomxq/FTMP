33a12a31aec947554ddd0694368b5c496efd109b
fotomxq <fotomxq@gmail.com>
2014-04-08 21:06:25 +0800

    取消自动登录

--- action-logged.php
+++ action-logged.php
@@ -3,7 +3,7 @@
 /**
  * 登录检测模块
  * @author fotomxq <fotomxq.me>
- * @version 3
+ * @version 4
  * @package web
  */
 //引用全局
@@ -11,11 +11,6 @@ require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'glob.php');
 
 //判断是否已经登录
 $userID = $user->logged($ipAddr);
-
-//Debug模式自动登录
-if(DEBUG_ON === true){
-    $userID = 1;
-}
 
 if ($userID < 1) {
     //如果没有登录，跳转到登录页面

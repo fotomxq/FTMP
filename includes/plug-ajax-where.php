3f92a66036d85a558da4b638f640e2bdef266fce
fotomxq <fotomxq@gmail.com>
2014-05-20 16:06:42 +0800

    修正where生成器错误

--- includes/plug-ajax-where.php
+++ includes/plug-ajax-where.php
@@ -11,10 +11,10 @@
 
 /**
  * Ajax-WHERE快速组合插件
- * @since 2
+ * @since 4
  * @param array $fields 对应匹配的字段组，eg: array('id','name',...)
  * @param array $wheres 客户端提交的where数组
- *                                                  eg: array(0=>array(0=>对应的键值 , 1=>匹配标记[包含/=/</>/<=/>=/!=/不包含] , 2=>值 , 3=>和上一个条件关系[or/and]),...)
+ *                                                  eg: array(0=>array(0=>对应的键值 , 1=>匹配标记健值[包含/=/</>/<=/>=/!=/不包含] , 2=>值 , 3=>和上一个条件关系[or/and]),...)
  *                                                  注,在[]括号内的内容对应数字值
  * @return array 生成的where和attrs
  */
@@ -22,10 +22,11 @@ function PlugAjaxWhere($fields, $wheres) {
     $where = '';
     $attrs = null;
     if (is_array($wheres) == true) {
-        foreach ($wheres as $v) {
+        foreach ($wheres as $k=>$v) {
             //必须确保键位存在，否则拒绝添加
             $fieldStr = isset($fields[$v[0]]) == true ? $fields[$v[0]] : '';
             if ($fieldStr) {
+                $fieldStrK = $fieldStr.$k;
                 //获取等式
                 $qs = array('LIKE','=', '<', '>', '<=', '>=', '!=','!=');
                 $q = isset($qs[$v[1]]) == true ? $qs[$v[1]] : $qs[$v[0]];
@@ -33,22 +34,22 @@ function PlugAjaxWhere($fields, $wheres) {
                 if ($where != '') {
                     //如果不是第一个条件，则添加and或or关系语句
                     if ($v[3] == 'and') {
-                        $where .= ' and `' . $fieldStr . '` ' . $q . ' :' . $fieldStr;
+                        $where .= ' and `' . $fieldStr . '` ' . $q . ' :' . $fieldStrK;
                     } else {
-                        $where .= ' or `' . $fieldStr . '` ' . $q . ' :' . $fieldStr;
+                        $where .= ' or `' . $fieldStr . '` ' . $q . ' :' . $fieldStrK;
                     }
                 } else {
                     //如果是第一个条件，则直接添加条件
-                    $where .= '`' . $fieldStr . '` ' . $q . ' :' . $fieldStr;
+                    $where .= '`' . $fieldStr . '` ' . $q . ' :' . $fieldStrK;
                 }
                 //过滤组，全部按照STR处理
                 $val = '';
-                if($q == 0 || $q == 7){
+                if($q == 'LIKE' || $q == '!='){
                     $val = '%' . $v[2] . '%';
                 }else{
                     $val = $v[2];
                 }
-                $attrs[':' . $fieldStr] = array($val, PDO::PARAM_STR);
+                $attrs[':' . $fieldStrK] = array($val, PDO::PARAM_STR);
             }
         }
     }
